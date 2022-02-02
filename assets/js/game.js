36489c2301a23400795ec4f7744d9ccbbd7180ca
M8MBA <sauce.ix@gmail.com>
2022-02-01 21:41:42 -0700

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,10 @@
+// Game States
+// "WIN" - Player robot has defeated all enemy robots
+//    * Fight all enemy robots
+//    * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
+
+
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
@@ -6,11 +13,25 @@ var playerMoney = 10;
 // You can also log multiple values at once like this 
 // console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 10;
 
-var fight = function() {
+/*
+console.log(enemyNames[0]);
+console.log(enemyNames[1]);
+console.log(enemyNames[2]);
+console.log(enemyNames.length);
+for(var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index");
+}
+*/
+
+
+
+var fight = function(enemyName) {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
@@ -22,7 +43,7 @@ var fight = function() {
         // remove enemy's health by subtracting the amount set in the playerAttack variable
         enemyHealth = enemyHealth - playerAttack;
         console.log(
-            playerName + " attatcked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining"
+            playerName + " attatcked " + enemyNames + ". " + enemyNames + " now has " + enemyHealth + " health remaining"
         );
 
     // check enemy's health
@@ -55,18 +76,14 @@ var fight = function() {
             window.alert(playerName + "has decided to skip this fight. Goodbye!");
             // subtract money from playerMoney for skipping
             playerMoney = playerMoney - 2;
-        }
+        }   
+        
         // if no (false), ask question again by running fight() again
-        else {
-            fight();
-        }
+       // else {
+        //    fight();
     }
+}
 
-
-fight();
-
-/*
-window.alert(playerName + " has chosen to skip the fight!");
-} else {
-    window.alert("You need to choose a valid option. Try again!");
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
 }
